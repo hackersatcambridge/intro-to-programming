dd8122efbf2a4ca7b18c84edfedd5f0e9d43bd44
Unknown <luciabura@gmail.com>
2017-11-12 11:18:53 +0000

    Corrected Todo.js

--- workshop-1/src/js/Todo.js
+++ workshop-1/src/js/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import todoList from './data.js'
 
 class Todo extends Component {
   /*This is required in order to locally save the To-Dos we will be working with
@@ -17,33 +18,28 @@ class Todo extends Component {
   // Loads the contents of an external, predefined todo list 
   // Adds these existing todos to a local list of todos
   loadTodos(){
-    alert("Not implemented yet!");
     //TODO: use your todoList, imported from `./data.js
-    // in conjuction with `storeTodo(content)` 
+    // in conjuction with `addTodo(name)` 
 
     // Your code goes here
-
   }
 
   // Takes a name (as a string) and adds it the local list of todos
   addTodo(name){    
     //TODO: create a new todo object and add it to the local list of todos
 
-
     // `storeTodo` takes ANYTHING (some content) and adds it to the local list. 
     // for the beginning, it's taking name as an input, you may want to change that. 
+
     this.storeTodo(name);
   }
 
   // Takes an id of a todo and removes it from the local list of todos
   removeTodo(id){
-    alert("Not implemented yet!");
     //TODO: remove the To-Do item with a matching id to the input one
     // You're working with the local To-Dos, these are held carefully
     // we're getting them with `getLocalTodos` helper function
     var toDos = this.getLocalTodos();
-
-    // Your code goes here
     
     // After we're done performing the removing on toDos, 
     // we can go ahead and update the local To-Dos  
@@ -51,8 +47,8 @@ class Todo extends Component {
   }
  
   // Takes an id of a todo and markes it as completed in the local list of todos
+  // This will be triggered when you click on a Todo item :) 
   markTodoCompleted(id){
-    alert("Not implemented yet!");
     //TODO: mark ToDo item with a matching id to the input one as completed 
     // Hint: At this point your local ToDos should already be objects
     var toDos = this.getLocalTodos();
@@ -76,13 +72,13 @@ class Todo extends Component {
 
   // Adds a To-Do Item to the web-content to abstract away these complications 
   showTodo(toDo){
-    return <div className="todo-item" onClick={()=>this.markTodoCompleted(toDo.id)}>
-      <div>{toDo.name}</div>
-      <button onClick={()=>this.removeToDo(toDo.id)}/>
+    return <div className="Todo_item" onClick={()=>this.markTodoCompleted(toDo.id)}>
+      <div className={toDo.completed == true? "Todo_completed" :"Todo_active"}>{toDo.name}</div>
+      <button className="Todo_remove" onClick={()=>this.removeTodo(toDo.id)}>X</button>
     </div>
   }
   // Takes as an input an array of To-Dos and overwrites the locally saved ones
-  updateToDos(todoList){
+  updateLocalTodos(todoList){
     this.setState({toDosLocal: todoList});
   }
   // Returns an array containing locally saved To-Dos 
@@ -92,19 +88,19 @@ class Todo extends Component {
   // =========================================================================
 
   // Add any additional helper functions here 
- 
+  
   // =========================================================================
 
   // This is where the magical appearing on the screen happens. 
   // Don't be taken aback by all the notations, it's nothing to write home about!
 
   render() {    
-    return <div className="ToDo"> 
-      <div className="title">{this.renderDescription()}</div>
-      <div id="todo-container">
+    return <div className="Todo"> 
+      <div className="Todo_title">{this.renderDescription()}</div>
+      <div className="Todo_container">
         <button className="Button" onClick={()=>this.loadTodos()}>Show Saved ToDos</button>
-        {this.state.toDosLocal.map((toDo,i) => toDo.value !== undefined? 
-          this.showTodoItem(toDo) : 
+        {this.state.toDosLocal.map((toDo,i) => toDo.name !== undefined? 
+          this.showTodo(toDo) : 
           <div key={i}>{toDo}</div>)}
       </div>       
     </div> 
