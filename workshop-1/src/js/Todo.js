185f9715d0e542a0abdf36ff1b51363b48233c64
Unknown <luciabura@gmail.com>
2017-11-12 11:07:18 +0000

    Added solution

--- workshop-1/src/js/Todo.js
+++ workshop-1/src/js/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import todoList from './data.js'
 
 class Todo extends Component {
   /*This is required in order to locally save the To-Dos we will be working with
@@ -17,33 +18,56 @@ class Todo extends Component {
   // Loads the contents of an external, predefined todo list 
   // Adds these existing todos to a local list of todos
   loadTodos(){
-    alert("Not implemented yet!");
     //TODO: use your todoList, imported from `./data.js
     // in conjuction with `storeTodo(content)` 
 
     // Your code goes here
-
+    for (var i = 0; i < todoList.length; i++){
+      this.addTodo(todoList[i]);
+    }
   }
 
   // Takes a name (as a string) and adds it the local list of todos
   addTodo(name){    
     //TODO: create a new todo object and add it to the local list of todos
 
-
     // `storeTodo` takes ANYTHING (some content) and adds it to the local list. 
     // for the beginning, it's taking name as an input, you may want to change that. 
-    this.storeTodo(name);
+    var localTodos = this.getLocalTodos();
+    var nextId;
+    
+    // If we're facing an empty to do list, out nextId should be 0, i.e the first id.
+    if(localTodos.length == 0){
+      nextId = 0;
+    }
+    else{
+      var lastElement = localTodos[localTodos.length-1];
+      nextId = lastElement.id + 1;
+    }
+
+    var toDo = {
+      name: name,
+      id: nextId,
+      completed: false
+    }
+
+    this.storeTodo(toDo);
   }
 
   // Takes an id of a todo and removes it from the local list of todos
   removeTodo(id){
-    alert("Not implemented yet!");
     //TODO: remove the To-Do item with a matching id to the input one
     // You're working with the local To-Dos, these are held carefully
     // we're getting them with `getLocalTodos` helper function
     var toDos = this.getLocalTodos();
 
     // Your code goes here
+    for (var i = 0; i<toDos.length; i++){
+      if (toDos[i].id == id){
+        toDos.splice(i, 1);
+        break;
+      }
+    }
     
     // After we're done performing the removing on toDos, 
     // we can go ahead and update the local To-Dos  
@@ -51,14 +75,25 @@ class Todo extends Component {
   }
  
   // Takes an id of a todo and markes it as completed in the local list of todos
+  // This will be triggered when you click on a Todo item :) 
   markTodoCompleted(id){
-    alert("Not implemented yet!");
     //TODO: mark ToDo item with a matching id to the input one as completed 
     // Hint: At this point your local ToDos should already be objects
     var toDos = this.getLocalTodos();
     
     // Your code goes here
-    
+    for (var i = 0; i<toDos.length; i++){
+      if(toDos[i].id === id){
+        toDos[i].completed = true;
+        /*
+        // If you want to toggle completed or not when you click on the object, 
+        // you might want to comment out the line above and try: 
+        toDos[i].completed = !toDos[i].completed;
+        // Observe the '!' there
+        */
+        break;
+      }
+    }
     this.updateLocalTodos(toDos);
   }
 
@@ -76,13 +111,13 @@ class Todo extends Component {
 
   // Adds a To-Do Item to the web-content to abstract away these complications 
   showTodo(toDo){
-    return <div className="todo-item" onClick={()=>this.markTodoCompleted(toDo.id)}>
-      <div>{toDo.name}</div>
-      <button onClick={()=>this.removeToDo(toDo.id)}/>
+    return <div className="Todo_item" onClick={()=>this.markTodoCompleted(toDo.id)}>
+      <div className={toDo.completed == true? "Todo_completed" :"Todo_active"}>{toDo.name}</div>
+      <button className="Todo_remove" onClick={()=>this.removeTodo(toDo.id)}>X</button>
     </div>
   }
   // Takes as an input an array of To-Dos and overwrites the locally saved ones
-  updateToDos(todoList){
+  updateLocalTodos(todoList){
     this.setState({toDosLocal: todoList});
   }
   // Returns an array containing locally saved To-Dos 
@@ -92,19 +127,32 @@ class Todo extends Component {
   // =========================================================================
 
   // Add any additional helper functions here 
- 
+  addInputField(){
+    var input;
+    var toDoInput = 
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            this.addTodo(input.value);
+            input.value = '';}}>
+            <input ref={node => {
+                input = node;}} />
+        </form>
+
+    return toDoInput;
+  }
   // =========================================================================
 
   // This is where the magical appearing on the screen happens. 
   // Don't be taken aback by all the notations, it's nothing to write home about!
 
   render() {    
-    return <div className="ToDo"> 
+    return <div className="Todo"> 
       <div className="title">{this.renderDescription()}</div>
-      <div id="todo-container">
+      {this.addInputField()}
+      <div className="Todo_container">
         <button className="Button" onClick={()=>this.loadTodos()}>Show Saved ToDos</button>
-        {this.state.toDosLocal.map((toDo,i) => toDo.value !== undefined? 
-          this.showTodoItem(toDo) : 
+        {this.state.toDosLocal.map((toDo,i) => toDo.name !== undefined? 
+          this.showTodo(toDo) : 
           <div key={i}>{toDo}</div>)}
       </div>       
     </div> 
